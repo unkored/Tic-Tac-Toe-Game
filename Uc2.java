ecbb786a716a8d8cda7ffb700d4a1480ffb80a88
thanga-111 <tt8772@srmist.edu.in>
2026-04-28 13:17:12 +0530

    feat : add Uc2

---
+++ Uc2.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+
+public class Uc2 {
+
+    static char[][] board = new char[3][3];
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    static void printBoard() {
+        System.out.println("-------------");
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        System.out.println("=== Toss Result ===");
+
+        if (isHumanTurn) {
+            System.out.println("Human plays first.");
+        } else {
+            System.out.println("Computer plays first.");
+        }
+
+        System.out.println("Human symbol: " + humanSymbol);
+        System.out.println("Computer symbol: " + computerSymbol);
+    }
+}
